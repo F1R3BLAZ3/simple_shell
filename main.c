2a0fb0ae5fec13d0f93113d0728ee4ecc24a3820
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-05-11 17:50:00 +0200

    Main Files Setup

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int main(int argc, char **argv)
+{
+	char *prompt = "$ ";
+	char *buf;
+	size_t n = 0;
+	(void)argc;
+	(void)argv;
+
+	printf("%s", prompt);
+	getline(&buf, &n, stdin);
+	printf("%s\n", buf);
+
+	free(buf);
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define _POSIX_C_SOURCE 200809L
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /* MAIN_H */
