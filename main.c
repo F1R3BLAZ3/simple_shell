55f9552cc22df9290cca980ab84b49160ec82df0
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-05-13 09:35:24 +0000

    Task 1 - Started with tokenization

--- main.c
+++ main.c
@@ -4,13 +4,15 @@ int main(int argc, char **argv)
 {
 	char *prompt = "hsh: $ ";
 	char *buf;
+	*buf_copy;
 	size_t n = 0;
 	ssize_t val;
 	(void)argc;
 	(void)argv;
+	const char *delim = " \n"
 
-	/* create an infinite loop */
-	while (1)
+	    /* create an infinite loop */
+	    while (1)
 	{
 		printf("%s", prompt);
 		val = getline(&buf, &n, stdin);
@@ -21,6 +23,13 @@ int main(int argc, char **argv)
 		}
 
 		printf("%s\n", buf);
+
+		buf_copy = malloc(sizeof(char) * val);
+		if (buf_copy == NULL)
+		{
+			perror("hsh: memory allocation error");
+			return (-1);
+		}
 
 		free(buf);
 	}
