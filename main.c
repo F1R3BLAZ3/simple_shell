9fa5e64e25175c20d81b7071a7a310211baf4c2d
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-05-15 12:58:45 +0000

    Added function descriptions - main

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
+
+/**
+  * main - Simple shell program
+  * @argc: Number of arguments passed to the program
+  * @argv: Array of strings representing the arguments passed to the program
+  * 
+  * Description: This function is the entry point for a simple shell program. It creates an infinite loop that
+  * prompts the user for input and executes commands entered by the user. The loop continues until
+  * the user exits the shell by typing 'exit' or by pressing Ctrl-D. The function prints a prompt
+  * 'hsh: $ ' to the standard output, reads a line of input from the user using the getline function,
+  * and tokenizes the input using the tokenize function. If the first token (command) is 'exit', the
+  * function returns. If the first token is NULL, the function continues to the next iteration of the
+  * loop. Otherwise, the function executes the command using the execute_command function. The function
+  * then frees the memory used by the tokens array and repeats the loop.
+  * 
+  * Return: This function returns 0 if the program exits successfully.
+  */
 int main(int argc, char **argv)
 {
 	char *prompt = "hsh: $ ";
