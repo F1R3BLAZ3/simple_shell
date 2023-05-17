72d86b3e9837311d67eef8a045097d44c97ccdce
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-05-17 15:39:36 +0000

    Finished Task 3

--- execute.c
+++ execute.c
@@ -50,11 +50,42 @@ void execute_command(char **tokens)
 	}
 }
 
+/**
+ * search_path - Search for a command in the directories specified by
+ * the PATH environment variable
+ * @tokens: A pointer to an array of strings (tokens) representing the command
+ * and its arguments
+ *
+ * Description: This function searches for a command in the directories
+ * specified by the PATH environment variable.
+ * It takes an array of strings representing the command and its arguments.
+ * If the first token in the array contains a forward slash ('/'),
+ * indicating a full path, the function returns a duplicate of the token.
+ * Otherwise, the function searches for the command in each directory
+ * specified by the PATH variable. It iterates through the directories,
+ * constructs a path by concatenating the directory and the command,
+ * and checks if the constructed path is a valid executable using the access
+ * function with the F_OK and X_OK flags. If a valid executable is found,
+ * the function returns a dynamically allocated string containing the full path
+ * to the command. If no valid executable is found, the function returns NULL.
+ * The function also handles memory allocation errors
+ * by printing an error message and exiting.
+ *
+ * Return: A dynamically allocated string containing the full path
+ * to the command if it is found in one of the directories specified by
+ * the PATH variable. If the command is not found or there is a memory
+ * allocation error, the function returns NULL.
+ */
 char *search_path(char **tokens)
 {
 	char *dir, *token, *path = getenv("PATH");
 	char *reversed_path = strdup(path);
 	char *token_end = reversed_path + strlen(reversed_path) - 1;
+
+	if (strchr(tokens[0], '/'))
+	{
+		return (strdup(tokens[0]));
+	}
 
 	token = strtok_r(reversed_path, PATH_SEPARATOR, &token_end);
 	while (token != NULL)
