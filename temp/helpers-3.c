87c31750cf775fadada0f39e3a1b81a12de0177d
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-05-25 00:45:46 +0200

    Revert "description of getline"
    This reverts commit 4dbe12cc5402b5c759df351242fe5fc43b61ed26.

--- temp/helpers-3.c
+++ temp/helpers-3.c
@@ -1,28 +1,5 @@
 #include "main.h"
 
-
-/**
- * _getline - Read a line of input from a file stream
- * @lineptr: Pointer to a buffer that will hold the read line
- * @n: Pointer to the size of the allocated buffer
- * @stream: The file stream to read from
- * Description: This function reads a line of input from the specified file
- * stream and stores it in the buffer pointed to by lineptr. The buffer is
- * dynamically resized as needed to accommodate the input. The size of the
- * allocated buffer is tracked by the n parameter.
- * he function reads data in
- * chunks from the stream using a static buffer to minimize the number of
- * read system calls
- * It continues reading until it encounters a newline
- * character ('\n') or reaches the end of the stream.
- * The final line, including the newline character, is stored in lineptr.
- * The function returns the number of characters read, excluding
- * the newline character, or -1 if an error occurs
- * or the end of the stream is reached before any characters are read.
- * Return: The number of characters read, excluding the newline character,
- * or -1 on failure or end of stream.
-*/
-
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
 	static char buffer[BUFSIZE];
